e0076536a6b632501a064e4a2041bc92fdd0b076
bluered <k1ll4h_tv3t@protonmail.com>
2023-06-18 21:22:09 +0700

    feat: patch method

--- src/main/java/su/puzzle/pay/api/PlasmoApi.java
+++ src/main/java/su/puzzle/pay/api/PlasmoApi.java
@@ -1,16 +1,18 @@
 package su.puzzle.pay.api;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse.BodyHandlers;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 import su.puzzle.pay.PuzzlePayMod;
 import su.puzzle.pay.api.exceptions.ApiCallException;
 import su.puzzle.pay.api.types.*;
@@ -19,73 +21,70 @@ public class PlasmoApi {
     public static final String API_URL = "https://rp.plo.su/api";
     public static String token = null;
 
+    private static HttpClient httpClient = HttpClient.newHttpClient();
+
     public static void setToken(String token) {
         PlasmoApi.token = token;
     }
 
     public static void assertTokenNotNull() {
         if (PlasmoApi.token == null || PlasmoApi.token.equals("")) {
-            throw new IllegalArgumentException("PlasmoApi.token must be set!");
+            throw new IllegalStateException("Token must be set!");
         }
     }
 
     public static Response<BankCardsResponse> getAllCards() throws ApiCallException {
-        Type type = new TypeToken<Response<BankCardsResponse>>() {}.getType();
+        Type type = new TypeToken<Response<BankCardsResponse>>() {
+        }.getType();
         return request("/bank/cards", "GET", type, null);
     }
 
-    public static Response<Object> transfer(int amount, String from, String message, String to) throws ApiCallException {
+    public static Response<Object> transfer(int amount, String from, String message, String to)
+            throws ApiCallException {
         TransferRequest req = new TransferRequest(amount, from, message, to);
-        Type type = new TypeToken<Response<Object>>() {}.getType();
+        Type type = new TypeToken<Response<Object>>() {
+        }.getType();
         return request("/bank/transfer", "POST", type, req);
     }
 
-    // TODO: пофиксить это говно, в .setRequestMethod() нет "PATCH"
-    /* public static Response<Object> updateUserActiveCard(BankCard card) throws ApiCallException {
+    public static Response<Object> updateUserActiveCard(BankCard card) throws ApiCallException {
         PatchCard req = new PatchCard("EB-" + card.id());
-        Type type = new TypeToken<Response<Object>>() {}.getType();
+        Type type = new TypeToken<Response<Object>>() {
+        }.getType();
         return request("/bank/cards/active", "PATCH", type, req);
-    } */
+    }
+
+    public static Response<Object> updateUserActiveCard(String card_id) throws ApiCallException {
+        PatchCard req = new PatchCard(card_id);
+        Type type = new TypeToken<Response<Object>>() {
+        }.getType();
+        return request("/bank/cards/active", "PATCH", type, req);
+    }
 
     public static Response<ProfileResponse> getUser() throws ApiCallException {
-        Type type = new TypeToken<Response<ProfileResponse>>() {}.getType();
+        Type type = new TypeToken<Response<ProfileResponse>>() {
+        }.getType();
         return request("/user", "GET", type, null);
     }
 
     public static String request(String endpoint, String method, String requestBody) throws ApiCallException {
         try {
-            URL url = new URL(API_URL.concat(endpoint));
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod(method);
-
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-
             assertTokenNotNull();
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            connection.setRequestProperty("Authorization", String.format("Bearer %s", PlasmoApi.token));
 
-            if (requestBody != null) {
-                try (OutputStream outStream = connection.getOutputStream()) {
-                    outStream.write(requestBody.getBytes(StandardCharsets.UTF_8));
-                } catch (Exception e) {
-                    PuzzlePayMod.LOGGER.warn(e.getMessage());
-                    throw new ApiCallException("Error while sending request", e);
-                }
+            Builder request = HttpRequest.newBuilder()
+                    .uri(new URI(API_URL.concat(endpoint)))
+                    .header("Content-Type", "application/json; charset=UTF-8")
+                    .header("Authorization", String.format("Bearer %s", PlasmoApi.token));
+
+            if (requestBody == null) {
+                request.method(method, BodyPublishers.noBody());
+            } else {
+                request.method(method, BodyPublishers.ofString(requestBody));
             }
 
-            // Получить ответ от connection'а
-            String responseBody;
+            HttpResponse<String> response = httpClient.send(request.build(), BodyHandlers.ofString());
 
-            try (InputStream inStream = connection.getInputStream()) {
-                responseBody = new String(inStream.readAllBytes(), StandardCharsets.UTF_8);
-            } catch (IOException exception) {
-                responseBody = new String(connection.getErrorStream().readAllBytes(), StandardCharsets.UTF_8);
-            }
-
-            return responseBody;
-        } catch (ApiCallException apiCallException) {
-            throw apiCallException;
+            return response.body();
         } catch (Exception e) {
             PuzzlePayMod.LOGGER.warn(e.getMessage());
             throw new ApiCallException(e.getMessage(), e);
@@ -100,7 +99,8 @@ public class PlasmoApi {
             return gson.fromJson(request(endpoint, method, (String) null), JsonObject.class);
     }
 
-    public static <B, T> T request(String endpoint, String method, Type responseType, B requestBody) throws ApiCallException {
+    public static <B, T> T request(String endpoint, String method, Type responseType, B requestBody)
+            throws ApiCallException {
         Gson gson = new Gson();
         String requestBodyString = null;
 
