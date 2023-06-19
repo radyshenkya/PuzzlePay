b6c93e570e6ec52165a27d608bbf45d8c63a7a68
MeiNanziiii <oplotDev@gmail.com>
2023-06-19 11:24:21 +0300

    Update BankScreen.java

--- src/main/java/su/puzzle/pay/gui/bank/BankScreen.java
+++ src/main/java/su/puzzle/pay/gui/bank/BankScreen.java
@@ -18,7 +18,7 @@ public class BankScreen extends BaseOwoScreen<FlowLayout> {
 
     public BankScreen(BankCard thisCard) throws ApiCallException, ApiResponseException {
         this.thisCard = thisCard;
-        cardList = new CustomDropdownComponent(Sizing.fill(25), Sizing.content(),
+        cardList = new CustomDropdownComponent(Sizing.content(), Sizing.content(),
                 Text.literal(this.thisCard == null ? "Выберите карту" : "EB-" + this.thisCard.id()), false);
 
         PlasmoApi.getAllCards().unwrap().cards().forEach((card) -> {
